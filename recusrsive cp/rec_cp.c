b75a103a2316d2335c409c6f4c94f30dab7bef47
mira2k <dammitblin41@gmail.com>
2017-05-24 15:37:14 +0300

    fixed conditional jump related bugs
    added symlink handling

--- recusrsive cp/rec_cp.c
+++ recusrsive cp/rec_cp.c
@@ -15,12 +15,16 @@
 #define EMALLOC 3
 
 #define EBADMD  4
+#define ECHMOD  5
 
 #define USE_STAT  256
 #define USE_LSTAT 257
 #define BUFF_SIZE 1024
 
-/* only absolute pathnames allowed, sorry*/
+#define WRITE_MODE O_WRONLY | O_CREAT | O_TRUNC
+#define PERM_MODE_1 S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH | S_IWOTH
+#define PERM_MODE_2 S_IRWXU|S_IRWXG|S_IRWXO
+/* only absolute pathnames are allowed, sorry*/
 
 int is_dir(const char* path, int mode)
 {
@@ -42,27 +46,26 @@ int is_dir(const char* path, int mode)
 
 int make_new_dir (char* path, char* name, char* new_path)
 {
-     
      char buff[PATH_MAX];
-     
+     char *end_ptr;
+     buff[0] = '\0';
      DIR *dp = NULL;
      strcpy(buff, path);
-     char *end_ptr = buff;
+     buff[strlen(path)] = '\0';
+     end_ptr = buff;
      
      if (NULL == (dp = opendir(path)) )
      {
-        
         printf("\n Cannot open Input directory [%s]\n",path);
+        closedir(dp);
         return ENOTDIR;
      }
      
      else
-     {
-        //printf("\n name[%s]\n",name);
-        //printf("\n name[%s]\n",buff); 
+     { 
         if(buff[strlen(buff)-1]=='/')
         {
-            //printf("\n bfr cpy name[%s]\n",buff);
+            /*printf("\n bfr cpy name[%s]\n",buff);*/
             
             end_ptr += strlen(buff);
             strcpy(end_ptr, name);
@@ -70,13 +73,13 @@ int make_new_dir (char* path, char* name, char* new_path)
             buff[z] = '/';
             ++z;
             buff[z] = '\0';
-            //printf("\n aftr cpy buff[%s]\n",buff);
+            /*printf("\n aftr cpy buff[%s]\n",buff);*/
         }
         else
         {
-            
             int z = strlen(buff);
             buff[z] = '/';
+            buff[z + 1] = '\0';
             end_ptr += strlen(buff);
             strcpy(end_ptr, name);
             z = strlen(buff);
@@ -85,32 +88,36 @@ int make_new_dir (char* path, char* name, char* new_path)
             buff[z] = '\0';
         }
 
-        //printf("\n Creating a new directory [%s]\n", buff);
-        mkdir(buff,S_IRWXU|S_IRWXG|S_IRWXO);
+        /*printf("\n Creating a new directory [%s]\n", buff);*/
+        mkdir(buff,PERM_MODE_2);
         
         strcpy(new_path, buff);
         
+        closedir(dp);
         return EOK;
     }
 }
-
 
 char* get_dir_name(const char* dir_path) 
 {
     char *buf = NULL;
     int count = 0, has_slash = 0;
+    int len = 0;
     int i = strlen(dir_path) - 1;
     if (dir_path[i] == '/') 
     {
         --i;
         has_slash = 1;
+        printf("has slash\n");
     }
+
     while ((i >= 0) && (dir_path[i] != '/'))
     {
         --i;
         ++count;
     }
-    buf = (char*) malloc (count + 1);
+    len = count;
+    buf = (char*) malloc (len + 1);
     if (buf == NULL)
     {
         printf ("Bad malloc(((");
@@ -122,10 +129,9 @@ char* get_dir_name(const char* dir_path)
         buf[count] = dir_path[i];
         ++count;
     }
-    buf[strlen(dir_path) - has_slash] = '\0';
+    buf[count] = '\0';
     return buf;
 }
-
 
 int file_to_file (const char* src, const char* dst) 
 {
@@ -140,8 +146,7 @@ int file_to_file (const char* src, const char* dst)
         printf("\nError opening file %s\n", src);
         return EBADMD;  
     }
-    destFD = open(dst,O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR | 
-    S_IRGRP | S_IWGRP | S_IROTH | S_IWOTH);
+    destFD = open(dst, WRITE_MODE, PERM_MODE_1);
  
     if(destFD == -1)
     {
@@ -153,7 +158,9 @@ int file_to_file (const char* src, const char* dst)
     while((nb_read = read(srcFD,buff,BUFF_SIZE)) > 0)
     {
         if(write(destFD,buff,nb_read) != nb_read)
+        {
             return EBADMD;
+        }
     }
     
     if(nb_read == -1) 
@@ -174,7 +181,53 @@ int file_to_file (const char* src, const char* dst)
         printf("\nError in closing file %s\n",dst);
         return EBADMD;
     }
- 
+    struct stat src_st;
+    if (stat(src, &src_st) < 0)
+        return ECHMOD;
+    if (chmod(dst, src_st.st_mode) < 0)
+        return ECHMOD;
+    return 0;
+    return EOK;
+}
+
+int handle_symlinks (char* f, char* ff, char* e_name)
+{   
+    struct stat info;
+    char * link;
+    int r_link;
+    link = NULL;
+    if (lstat(f, &info))
+    {
+        if (link != NULL) free(link);
+        return EBADMD;
+    }
+    
+    link = (char*)malloc(info.st_size + 1);
+    if (link == NULL)
+    {
+        if (link != NULL) free(link);
+        return EMALLOC;
+    }
+    
+    r_link = readlink(f, link, info.st_size + 1);
+    if (r_link < 0)
+    {
+    
+        if (link != NULL) free(link);
+        return EBADMD;
+    }
+    link[info.st_size] = '\0';
+    
+    if (symlink(link ,ff) == -1)
+    {
+        if (link != NULL)
+        free(link);
+        return EBADMD;
+    }
+    
+    if (link != NULL)
+        free(link);
+
     return EOK;
 }
 
@@ -182,66 +235,76 @@ int rec_dir_cp(char* source, char* dest, char* name)
 {
     
     char new_dest[PATH_MAX];
-    int err = make_new_dir(dest, name, new_dest);
-    
-    if (err != EOK) return err;
-    DIR *dir = opendir(source); //Assuming absolute pathname here.
-    if (dir == NULL) return ENOTDIR;
-    
-    char Path[PATH_MAX], *EndPtr = Path;
+    int err;
+    int control = EOK;
+    char Path[PATH_MAX];
     struct dirent *e;
+    err = make_new_dir(dest, name, new_dest);
+    if (err != EOK) return err;
+    DIR *dir = opendir(source);
+    /*Assuming absolute pathname here.*/
+    if (dir == NULL) return ENOTDIR;
     strcpy(Path, source);  
-    //printf("Path %s\n", Path);
-   
-    EndPtr += strlen(Path); 
+    /*printf("Path %s\n", Path);*/
     
-    while((e = readdir(dir)) != NULL) //Iterates through the entire directory.
+    while (((e = readdir(dir)) != NULL) && control == EOK)
+    /*Iterates through the entire directory.*/
     {  
         struct stat info;     
-        char f[strlen(e->d_name)* sizeof(char) + strlen(Path) + 3];
+        char f[(strlen(e->d_name) + strlen(Path) + 3)];
+        int res = EOK;
         sprintf(f, "%s/%s", Path, e->d_name);
-        //printf("\n rdc: let's see what %s file hides\n", f);      
         if(!stat(f, &info))
         {
             char e_name[PATH_MAX];
             strcpy(e_name, e->d_name);
             e_name[strlen(e_name)] = '\0';
             
-            if(is_dir(f, 256)) //Are we dealing with a directory?
+            if(is_dir(f, 256))
             { 
                 if (e_name[0] != '.')
-                    rec_dir_cp(f, new_dest, e_name); //Calls this function AGAIN, this time with the sub-name. 
+                    res = rec_dir_cp(f, new_dest, e_name);
+                control = res;
             } 
             
-            else if (S_ISREG(info.st_mode) )//Or did we find a regular file?
+            else if (S_ISREG(info.st_mode) )
             { 
                 char ff[PATH_MAX];
-                //printf("\ndest directory %s\n ",new_dest);
                 sprintf(ff, "%s%s", new_dest, e_name);
-                //printf("\ndest %s\n", ff);
-                if ((e_name)[0] != '.')
-                    file_to_file(f, ff);
+                ff[strlen(ff)] = '\0';
+                if ((e_name)[0] != '.') {
+                    res = file_to_file(f, ff);
+                }
+                control = res;
             }
-            
+            else if (S_ISLNK(info.st_mode)) {
+                char ff[PATH_MAX];
+                sprintf(ff, "%s%s", new_dest, e_name);
+                ff[strlen(ff)] = '\0';
+                res = handle_symlinks(f, ff, e_name);
+                control = res;
+            }
         }
     }
     closedir(dir);
-    return EOK;
+    return control;
 }
 
-int main(int argc, char** argv) {
-    if (argc < 3) {
-        printf("not enough args\n");
+int main(int argc, char** argv)
+{
+    if (argc < 3) 
+    {
+        printf("\nrcp: \nInput format: 'source directory path, destination directory path'\n");
+        printf("\nNotice that only absolute pathnames are allowed\n");
         return ENOARG;
     }
-    
     char* name = get_dir_name(argv[1]);
     if (name == NULL)
         return EMALLOC;
     int err = rec_dir_cp(argv[1], argv[2], name);
     free(name);
+    
     if (err)
         return err;
     return 0;
 }
-	
